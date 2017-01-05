8bac9e58176d78cf536f3b98c35a62bb358b89eb
te-te <te-te@naver.com>
2017-01-05 21:27:09 +0900

    view, data-binding

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/{foo?}', function ($foo = 'bar') {
 })->where('foo', '[0-9a-zA-Z{3}]');
 */
 
+/*
 Route::get('/', [
 	'as' => 'home',
 	function () {
@@ -34,4 +35,21 @@ Route::get('/', [
 
 Route::get('/home', function () {
 	return redirect(route('home'));
+});
+*/
+
+/*
+Route::get('/', function () {
+    return view('welcome')->with([
+        'name' => 'Foo',
+        'greeting' => '안녕하세요?',
+    ]);
+});
+*/
+
+Route::get('/', function () {
+    return view('welcome', [
+        'name' => 'Foo',
+        'greeting' => '안녕하세요??',
+    ]);
 });
